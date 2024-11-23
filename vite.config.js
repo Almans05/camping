d5d2c90f3f3f45b61fe1cadddb05b10599150874
Ali <tyronsanti@hotmail.com>
2024-11-23 18:15:59 -0500

    corrigo error main.js

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default {
         rollupOptions: {
             input: {
                 contacto: resolve(__dirname, 'pages/¿Como llegar?/index.html'),
-                productos: resolve(__dirname, 'pages/Medios de contacto/index.html'),
                 servicios: resolve(__dirname, 'pages/Tarifas/index.html'),
+                productos: resolve(__dirname, 'pages/Medios de contacto/index.html'),
             }
         }
     }
