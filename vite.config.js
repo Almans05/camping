abe36b9f9fd147f44641866fbe21913d0c38922d
Ali <tyronsanti@hotmail.com>
2024-11-22 23:30:18 -0500

    pages

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,7 @@ export default {
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname,'index.html'),
                 contacto: resolve(__dirname, 'pages/¿Como llegar?/index.html'),
-                nosotros: resolve(__dirname, 'pages/Descuentos/index.html'),
                 productos: resolve(__dirname, 'pages/Medios de contacto/index.html'),
                 servicios: resolve(__dirname, 'pages/Tarifas/index.html'),
             }
